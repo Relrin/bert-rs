de4e01da226a18c6a8ddfd705fa7ae613871d13c
Valeryi Savich <Relrin78@gmail.com>
2016-08-03 21:19:28 +0300

    Implemented trait for small integer

--- src/serializers.rs
+++ src/serializers.rs
@@ -4,7 +4,7 @@ use std::vec::Vec;
 // use anymap::AnyMap;
 // use std::any::Any;
 
-use types::BertTag;
+use types::{BERT_LABEL, BertTag};
 
 
 pub struct Serialiazer {
@@ -27,20 +27,52 @@ pub trait Deserialize<T> {
 }
 
 
-impl Serialize<String> for Serialiazer {
-    fn to_bert(&self, data: String) -> Vec<u8> {
-        let tag = BertTag::String as u8;
+impl Serialiazer {
+    fn generate_term(&self, tag: BertTag, data: Vec<u8>) -> Vec<u8> {
+        let mut binary = vec![tag as u8];
+        binary.extend(data.iter().clone());
+        binary
+    }
+
+    fn convert_string_to_binary(&self, data: String) -> Vec<u8> {
         let binary_string = data.into_bytes();
         let binary_length = binary_string.len() as u8;
-
-        let mut binary: Vec<u8> = vec![tag, 0, binary_length];
+        let mut binary = vec![0u8, binary_length];
         binary.extend(binary_string.iter().clone());
-
         binary
+    }
+
+    fn merge_atoms(&self, atom_1: Vec<u8>, atom_2: Vec<u8>) -> Vec<u8> {
+        let mut binary: Vec<u8> = atom_1.clone();
+        binary.extend(atom_2.iter().clone());
+        binary
+    }
+
+    fn get_bert_atom(&self) -> Vec<u8> {
+        let bert_string = BERT_LABEL.to_string();
+        let binary_string = self.convert_string_to_binary(bert_string);
+
+        self.generate_term(BertTag::Atom, binary_string)
     }
 }
 
 
-impl Deserializer {
+impl Serialize<String> for Serialiazer {
+    fn to_bert(&self, data: String) -> Vec<u8> {
+        let binary_string = self.convert_string_to_binary(data);
+        self.generate_term(BertTag::String, binary_string)
+    }
+}
 
+
+impl Serialize<bool> for Serialiazer {
+    fn to_bert(&self, data: bool) -> Vec<u8> {
+        let boolean_atom = data.to_string();
+        let binary_boolean = self.convert_string_to_binary(boolean_atom);
+
+        let bert_term = self.get_bert_atom();
+        let boolean_term = self.generate_term(BertTag::Atom, binary_boolean);
+
+        self.merge_atoms(bert_term, boolean_term)
+    }
 }
