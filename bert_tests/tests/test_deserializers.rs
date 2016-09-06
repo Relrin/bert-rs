de41cd71847a725314132e6a4f5fff73302caade
Valeryi Savich <Relrin78@gmail.com>
2016-09-06 20:51:36 +0300

    Fixed tests for u8 and i32 types

--- bert_tests/tests/test_deserializers.rs
+++ bert_tests/tests/test_deserializers.rs
@@ -5,10 +5,21 @@ use bert::{ETF_VERSION, binary_to_term};
 
 #[test]
 fn test_deserialize_u8() {
-    let mut data = vec![ETF_VERSION, 97, 100];
+    let data = vec![ETF_VERSION, 97, 100];
 
     assert_eq!(
-        binary_to_term(&data).unwrap(),
-        100u8
+        100u8,
+        binary_to_term(&data).unwrap()
+    )
+}
+
+
+#[test]
+fn test_deserialize_i32() {
+    let data = vec![ETF_VERSION, 98, 0, 0, 2, 0];
+
+    assert_eq!(
+        512i32,
+        binary_to_term(&data).unwrap()
     )
 }
